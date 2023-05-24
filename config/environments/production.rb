35215f16dcccab1de1c376313cf61695c7595f84
johnnybuma <johnny.buma@gmail.com>
2023-05-23 19:19:48 -0700

    Added app url to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = "www.nodeview.app"
+  Rails.application.routes.default_url_options[:protocol] = 'https'
+
   #Rails.application.routes.default_url_options[:host] = "your.domain.com"
   #Rails.application.routes.default_url_options[:protocol] = 'https'
 
