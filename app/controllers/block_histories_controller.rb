e00985c1ff6e829fc5a2087e42efa044259a8b78
Jonathan Buma <jonathanbuma@Jonathans-MacBook-Pro-2.local>
2023-12-19 19:56:50 -0800

    Updated chart view to handle large datasets

--- app/controllers/block_histories_controller.rb
+++ app/controllers/block_histories_controller.rb
@@ -1,23 +1,24 @@
 # app/controllers/block_histories_controller.rb
 class BlockHistoriesController < ApplicationController
   def index
-    start_block = params[:start_block] || BlockHistory.minimum(:block_number)
-    end_block = params[:end_block] || BlockHistory.maximum(:block_number)
+    start_block = params[:start_block].to_i
+    end_block = params[:end_block].to_i
 
-    block_histories = BlockHistory.where(block_number: start_block..end_block)
+    range = [end_block - start_block, 1].max
+    data_points = 1000 # This is the number of points you want to plot.
+    interval = [1, (range.to_f / data_points).ceil].max
 
-    @chart_data = block_histories.map do |block_history|
-      begin
-        data = JSON.parse(block_history.data)
-        {
-          block_number: block_history.block_number,
-          difficulty: data['result']['difficulty'].to_f,  # Assumes 'difficulty' is a top-level key
-          chainwork: data['result']['chainwork'].to_f     # Assumes 'chainwork' is a top-level key
-        }
-      rescue JSON::ParserError, TypeError
-        # Handle JSON parsing errors or return a default structure
-        { block_number: block_history.block_number, difficulty: nil, chainwork: nil }
-      end
+    @block_histories = BlockHistory
+                         .where(block_number: start_block..end_block)
+                         .where('block_number % ? = 0', interval)
+                         .order(:block_number)
+
+    @chart_data = @block_histories.pluck(:block_number, :data).map do |block_number, data_json|
+      data = JSON.parse(data_json)
+      response = data['result'] || {}
+      difficulty = response['difficulty'] || 0
+      chainwork = response['chainwork'] || 0
+      [block_number, difficulty.to_f, chainwork.to_f]
     end
   end
 end
